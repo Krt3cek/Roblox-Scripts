0c93767d061bb7d69335e015980434f251b671b3
Krt3cek <filipcipro@seznam.cz>
2024-10-18 16:21:07 +0200

    Create parkour.lua

---
+++ parkour.lua
@@ -0,0 +1,15 @@
+if 2329123383 ~= game.PlaceId then 
+    warn("#### - This Script is not for this game. - ####")
+    script:Destroy()
+    return 
+end
+local lp = game:GetService("Players").LocalPlayer
+local char = lp.Character or lp.CharacterAdded:Wait()
+local hrp = char:WaitForChild("HumanoidRootPart")
+local ws = game:GetService("Workspace")
+local stage = lp:WaitForChild("leaderstats").Stage
+repeat
+    task.wait()
+    firetouchinterest(hrp, ws[tostring(stage.Value + 1)], 0)
+until stage.Value >= 249
+hrp.CFrame = ws["250"].CFrame
